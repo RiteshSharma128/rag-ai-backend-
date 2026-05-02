67a2235f88501f730190513f680379a5b6814b68
Ritesh Sharma <rs3907367@gmail.com>
2026-05-02 11:20:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ connectRedis();
 // Security Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin:  "https://rag-ai-frontend-ashy.vercel.app" || 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
